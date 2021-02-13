a8f78d94cc39d87c699f6064b6aadf992fbc9043
NZWebDesign <65640337+NZWebDesign@users.noreply.github.com>
2021-02-14 09:29:23 +1300

    create javascript for fade ins

---
+++ script/app.js
@@ -0,0 +1,19 @@
+/*
+$(document).ready(function(){
+  $("button").click(function(){
+
+    $("#mainPhotoFade").fadeIn(3000);
+  });
+});
+*/
+
+$(document).ready(function(){
+      $("#photoFade").fadeIn(2500);
+    });
+
+    $(document).ready(function(){
+        $("#blogFade").fadeIn(1500);
+      });
+
+    
+
